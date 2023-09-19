b1089ef6e66c11fcd748ef68044b1d6af733e1ca
Felipe Merchan Diaz <felipe280596merchan@gmail.com>
2023-09-18 20:03:33 -0500

    .

---
+++ Ejercicio4.js
@@ -0,0 +1,6 @@
+//4. Escribir un programa que sume, reste, multiplique y divida dos números:
+//x = 10 y = 2.
+
+x=10;
+y=2;
+console.log("suma",x+y,"resta",x-y,"multiplicacion",x*y,"Division",x/y);
---
+++ Ejercicio5.js
@@ -0,0 +1,10 @@
+//5. Escribir un programa que sume, reste, multiplique y divida dos números
+//leídos desde el teclado.
+
+x=parseInt(prompt("introduce el primer numero:"));
+y=parseInt(prompt("introduce el segundo numero:"));
+document.write("suma",x+y,"resta",x-y,"multiplicacion",x*y,"Division",x/y);
+
+
+
+
